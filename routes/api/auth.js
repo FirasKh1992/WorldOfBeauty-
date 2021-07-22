b2c6cdf5f3c438e7d5ad548082763604b4e4f2b4
FirasKh1992 <firask53@gmail.com>
2021-07-22 12:34:56 +0300

    register & get authenticated user

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,78 @@
 const express=require('express');
-const router = express.Router();
+const router=express.Router();
+const auth=require('../../middleware/auth');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const { check, validationResult } = require('express-validator');
+const config=require('config');
+const bcrypt=require('bcryptjs');
 
+//@route GET api/auth
+//@desc get authenticate user
+//@access  private 
+router.get('/',auth, async (req,res)=>{
+try{
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+}catch(err){
+    console.error(err.message);
+    res.status(500).send('Server Error');
+}
+}
+);
 
-//@route  GET api/auth
-//@desc   Test route
-//@aceess Public -> there is no need for access token to use this route  
-router.get('/',(req,res)=>{
-    res.send('get request from auth');
-})
-module.exports = router;
+//@route post api/auth
+//@desc authenticate a user & get token 
+//@access  public
+router.post(
+    '/',
+    [
+      check('email', 'please include a valid email').isEmail(),
+      check(
+        'password',
+        'Password is required'
+      ).exists(),
+    ],
+    async (req, res) => {
+      const errors = validationResult(req);
+      if (!errors.isEmpty())
+        return res.status(400).json({ errors: errors.array() });
+      const {  email, password } = req.body;
+      try {
+        //see if user exists
+        let user = await User.findOne({ email });
+  
+        if (!user) {
+          return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+        
+        const isMatch=await bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res
+            .status(400)
+            .json({ errors: [{ msg: 'Invalid Credentials' }] });
+        }
+
+        const payload={
+            user:{
+                id:user.id
+            }
+        }
+        jwt.sign(
+            payload,
+            config.get('jwtSecret'),
+            {expiresIn:3600},
+            (err,token)=>{
+                if(err) throw err;
+                res.json({token});
+            })
+      } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+      }
+    }
+  );
+module.exports=router;
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,23 +1,74 @@
-const express=require('express');
+const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const { check, validationResult } = require('express-validator');
+const bcrypt=require('bcryptjs');
+const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
+const config=require('config');
 
+//@route post api/users
+//@desc Register User
+//@access  public
+router.post(
+  '/',
+  [
+    check('name', 'name is required').not().isEmpty(),
+    check('email', 'please include a valid email').isEmail(),
+    check(
+      'password',
+      'please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+    const { name, email, password } = req.body;
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
 
-//@route  GET api/users
-//@desc   Test route
-//@aceess Public -> there is no need for access token to use this route  
-router.get('/',(req,res)=>{
-    res.send('get request from users');
-})
+      if (user) {
+        return res.status(400).json({ errors: [{ msg: 'user already exist' }] });
+      }
+      const avatar=gravatar.url(email,{
+          s:"200",
+          r:"pg",
+          d:"mm"
+      })
+      user = new User({
+          name,
+          email,
+          avatar,
+          password
+      })
 
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password,salt);
+      await user.save();
+      //get users gravatar
+      //Encrypt  password
+      //return jsonwebtoken and that in order to keep the user logged in after the registeration.
 
-//@route  GET api/users
-//@desc   Test route
-//@aceess Public -> there is no need for access token to use this route  
-router.get('/',(req,res)=>{
-    res.send('get request from users');
-})
-
-
-
+      const payload={
+          user:{
+              id:user.id
+          }
+      }
+      jwt.sign(
+          payload,
+          config.get('jwtSecret'),
+          {expiresIn:3600000},
+          (err,token)=>{
+              if(err) throw err;
+              res.json({token});
+          })
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;

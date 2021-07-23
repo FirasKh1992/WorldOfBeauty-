e1285f2bb79e88a5616c6d9bcc6564f419c7c1da
FirasKh1992 <firask53@gmail.com>
2021-07-23 13:01:15 +0300

    add Login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
+  const [formData, setFormdata] = useState({
+    email: '',
+    password: '',
+  });
+  const { email, password } = formData;
+  const onChange = e =>
+    setFormdata({ ...formData, [e.target.name]: e.target.value });
+  const onSubmit = e => {
+    e.preventDefault();
+      console.log(formData);
+  };
   return (
-    <div>
-      <h1>Login</h1>
-    </div>
+    <Fragment>
+      <h1 className='large text-primary'>Sign In</h1>
+      <p className='lead'>
+        <FontAwesomeIcon icon={faUser} /> Sign into Your Account
+      </p>
+      <form className='form' onSubmit={e => onSubmit(e)}>
+        <div className='form-group'>
+          <input
+            type='email'
+            placeholder='Email Address'
+            name='email'
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className='form-group'>
+          <input
+            type='password'
+            placeholder='Password'
+            name='password'
+            value={password}
+            onChange={e => onChange(e)}
+            minLength='6'
+            required
+          />
+        </div>
+        <input type='submit' className='btn btn-primary' value='Login' />
+      </form>
+      <p className='my-1'>
+        Don't  have an account?{' '}
+        <Link to='/register'>
+          {' '}
+          <b>Sign Up</b>
+        </Link>
+      </p>
+    </Fragment>
   );
 };
 

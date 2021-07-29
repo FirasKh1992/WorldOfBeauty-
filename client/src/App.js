151878237b1a01e29d6d3ec20f459b8d66098e8f
FirasKh1992 <firask53@gmail.com>
2021-07-29 13:25:14 +0300

    add privateRoute for dashboard page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import Landing from './components/layouts/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layouts/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -32,6 +34,7 @@ function App() {
             <Switch>
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
+              <PrivateRoute exact path='/dashboard' component={Dashboard} />
             </Switch>
           </section>
         </Fragment>
